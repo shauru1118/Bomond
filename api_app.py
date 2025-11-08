d8cfa7f9653d628ef31f769e0f417e240321029d
shauru1118 <sargsyan_ae09@mail.ru>
2025-11-08 17:25:31 +0300

    add tree

--- api_app.py
+++ api_app.py
@@ -60,5 +60,28 @@ def get_files():
 def serve_file(date, subject, filename):
     return flask.send_from_directory(os.path.join(UPLOAD_DIR, date, subject), filename)
 
+@app.route("/tree")
+def get_tree():
+    def build_tree(path):
+        result = {"name": os.path.basename(path), "type": "folder", "children": []}
+        try:
+            for item in sorted(os.listdir(path)):
+                full = os.path.join(path, item)
+                if os.path.isdir(full):
+                    result["children"].append(build_tree(full))
+                else:
+                    result["children"].append({
+                        "name": item,
+                        "type": "file",
+                        "url": full.replace(os.getcwd(), "").replace("\\", "/")
+                    })
+        except Exception:
+            pass
+        return result
+
+    tree = build_tree(UPLOAD_DIR)
+    return flask.jsonify(tree)
+
+
 if __name__ == "__main__":
     app.run("0.0.0.0", 5000, debug=True)
